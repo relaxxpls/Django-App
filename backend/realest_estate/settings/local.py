efc9b70d92742f9d7bf1e53e8963f81d9e30048e
Laxman Desai <lolopaw24@gmail.com>
2021-05-29 10:49:57 +0530

    📦 NEW: Init django backend model

---
+++ backend/realest_estate/settings/local.py
@@ -0,0 +1,4 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-6(5%-d=pq@jc+i_^nw5md=&@xa!%vdx69r(ek!xmkt#^$pjtj)'
+
+PSQL_USER = {'USER': 'postgres', 'PASSWORD': 'password', 'HOST': 'localhost'}
