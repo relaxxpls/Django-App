1224a6e7a0b67275783dd4a9d39a1fa20a19c7fa
Laxman Desai <lolopaw24@gmail.com>
2021-05-31 11:13:18 +0530

    👌 IMPROVE: Setup contacts django app

--- backend/contacts/views.py
+++ backend/contacts/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.core import mail
+from rest_framework import permissions
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Contact
 
-# Create your views here.
+
+class ContactCreateView(APIView):
+    permission_classes = (permissions.AllowAny, )
+    
+    def post(self, request, format=None):
+        data = self.request.data
+        try:
+            email_message = (f"Name: {data['name']}\n"
+                             f"Email: {data['email']}\n\n"
+                             f"Message:\n {data['message']}")
+            mail.send_mail(data['subject'],
+                           email_message,
+                           '[YOUR SENDER EMAIL FROM YOUR SETTINGS]',
+                           ['[EMAIL YOU ARE SENDING TO]'],
+                           fail_silently=False)
+            mail.send_mail(subject, message, from_email, recipient_list)
+            return Response()
+        except:
+            return Response()
